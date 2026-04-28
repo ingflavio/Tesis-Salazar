7fc42aeaa4a73f5bda5c65be3a610a93e3c3a811
Jose Olival <joseandresolival@gmail.com>
2026-04-28 13:38:54 -0400

    recomendation component added

--- Front/src/pages/MainPage.jsx
+++ Front/src/pages/MainPage.jsx
@@ -1,3 +1,41 @@
+import { useState } from "react"
+import classes from '../styles/home.module.scss'
+import Icons from "../components/Icons"
+
+function Recomendation(){
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleChange = (e) => {
+    const checked = e.target.checked;
+    setIsChecked(checked);
+  };
+
+  return (<section className={classes.recomendation}>
+    <div className={classes.recomendation__header}>
+      <label className={`${classes.unfold_label} ${isChecked ? classes.unfolded : ''}`}>
+        <Icons icon='chevron' />
+        <input 
+          type="checkbox" 
+          className={classes.unfold_checkbox} 
+          checked={isChecked}
+          onChange={handleChange}
+        />
+      </label>
+      <label className="toDo">
+        <span>
+          <input type="checkbox" />
+          <Icons icon='check' />
+        </span>
+        <h3>Texto</h3>
+      </label>
+    </div>
+    <div className={`${classes.recomendation__body} ${isChecked ? classes.expanded : ''}`}>
+      <h4>Musculo tal</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus ipsa ex pariatur repellat ratione fuga error eum nulla dolore distinctio consequuntur, qui dolorum quis temporibus. Sunt atque et veritatis ducimus!</p>
+    </div>
+  </section>)
+}
+
 function useRecomendation(days){
   const fiveDaysRoutines = [
     {
@@ -57,26 +95,26 @@ function useRecomendation(days){
 }
 
 export function MainPage() {
-  let days = 0
+  const [days, setDays] = useState(0) 
 
-  // const 
+  const handleChange = (event) => {
+    setDays(event.target.value)
+  }
 
   return (
     <>
       <h1>Principal</h1>
       <div>
-         {
-          days === 0 
-          ? <label> ¿Cuantos dias entrenas por semana?
-              <select >
-                <option value="" disabled selected ></option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-              </select>
-            </label>
-          : <p>Juegito</p>
-         }
+        <label> ¿Cuantos dias entrenas por semana?
+          <select defaultValue='0' onChange={handleChange}>
+            <option value="0" disabled></option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </label>
+        <h1>{days}</h1>
+        <Recomendation />
       </div>
     </>
   )
