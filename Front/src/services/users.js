8071f6eb5f1a67bbb3effd74fa833e800222b464
Jose Olival <joseandresolival@gmail.com>
2026-06-24 14:47:48 -0400

    users services updated

--- Front/src/services/users.js
+++ Front/src/services/users.js
@@ -1,9 +1,23 @@
-export const searchUsers = () => {
-  //Aqui iria un fetch 
-  return users 
-}
+import { apiClient } from "./api";
 
-export const searchUser = (id) => {
-  //Aqui iria un fetch 
-  return users.filter((item) => item.id === id)[0]
-}
+export const userService = {
+  getUsers: () => apiClient.get('users'),
+  getUser: (id) => apiClient.get(`users/${id}`),
+  registerUser: ({ id, username, password, rol }) => apiClient.post(`users/register`,{
+    "cedula": id,
+    "name": username,
+    "password": password,
+    "rol": rol
+  }),
+  addProfile: ({id, name, lastName, email, phone, age, height_Cm, init_weight_kg, condition='ninguna' }) => apiClient.post(`users/${id}/details`,{
+    "firstName": name,
+    "lastName": lastName,
+    "email": email,
+    "phone": phone,
+    "age": age,
+    "height_Cm": height_Cm,
+    "init_weight_kg": init_weight_kg,
+    "condition":  condition
+  })
+
+} 
