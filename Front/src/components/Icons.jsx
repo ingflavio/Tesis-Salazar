e013d51ca7d4845c4a9c687607146bb4740c8d6f
Jose Olival <joseandresolival@gmail.com>
2026-06-27 14:27:20 -0400

    Icon bars added

--- Front/src/components/Icons.jsx
+++ Front/src/components/Icons.jsx
@@ -130,6 +130,14 @@ function Eye(){
   )
 }
 
+function Bars() {
+  return (
+    <svg viewBox="0 0 448 512">
+      <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/>
+    </svg>
+  )
+}
+
 
 export function Icons({icon}) {
   const icons = {
@@ -147,7 +155,8 @@ export function Icons({icon}) {
     'search': <SearchIcon />,
     'chevron': <ChevronICon />,
     'triangle': <Triangle/>,
-    'eye': <Eye />
+    'eye': <Eye />,
+    'bars': <Bars />
   }
 
   return icons[icon]
